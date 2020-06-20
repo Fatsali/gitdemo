432d3f717ca2a60ebd885cde313b923f955611ce
Fatih <fatih.akyazi.nbg@gmail.com>
2020-06-20 23:15:47 +0200

    ilk gönderdiğim mesaj

---
+++ src/git_test1/GitDemo.java
@@ -0,0 +1,10 @@
+package git_test1;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Bugünü anlayabilecem mi acaba?");
+		System.out.println("Çalýþýrsam anlarým inþallah.");
+	}
+
+}
